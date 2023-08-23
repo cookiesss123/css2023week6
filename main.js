2eb52fcf1a9ed9ab765e52a475d7106308367a58
cookiesss123 <hoce527@gmail.com>
2023-08-23 20:52:03 +0800

    加入aos動畫效果

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-console.log("Hello world!");
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+AOS.init();
